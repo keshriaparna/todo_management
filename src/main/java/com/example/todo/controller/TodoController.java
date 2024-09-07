622bf4dd8b0e919515c613389d681571072d07f0
Aparna Keshri <keshriaparna3@gmail.com>
2024-09-07 21:45:21 +0530

    Todo Management Rest APIs

---
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -0,0 +1,68 @@
+package com.example.todo.controller;
+
+import com.example.todo.dto.TodoDto;
+import com.example.todo.service.TodoService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/todos")
+public class TodoController {
+
+    private TodoService todoService;
+
+    @PostMapping
+    public ResponseEntity<TodoDto> addTodo(@RequestBody TodoDto todoDto){
+
+        TodoDto savedTodo = todoService.addTodo(todoDto);
+
+        return new ResponseEntity<>(savedTodo, HttpStatus.CREATED);
+    }
+
+    // Build Get Todo REST API
+    @GetMapping("{id}")
+    public ResponseEntity<TodoDto> getTodo(@PathVariable("id") Long todoId){
+        TodoDto todoDto = todoService.getTodo(todoId);
+        return new ResponseEntity<>(todoDto, HttpStatus.OK);
+    }
+
+    // Build Get All Todos REST API
+    @GetMapping
+    public ResponseEntity<List<TodoDto>> getAllTodos(){
+        List<TodoDto> todos = todoService.getAllTodos();
+        //return new ResponseEntity<>(todos, HttpStatus.OK);
+        return ResponseEntity.ok(todos);
+    }
+
+    // Build Update Todo REST API
+    @PutMapping("{id}")
+    public ResponseEntity<TodoDto> updateTodo(@RequestBody TodoDto todoDto, @PathVariable("id") Long todoId){
+        TodoDto updatedTodo = todoService.updateTodo(todoDto, todoId);
+        return ResponseEntity.ok(updatedTodo);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteTodo(@PathVariable("id") Long todoId){
+        todoService.deleteTodo(todoId);
+        return ResponseEntity.ok("Todo deleted successfully!.");
+    }
+
+    // Build Complete Todo REST API
+    @PatchMapping("{id}/complete") //we use patch to update existing resource partially whereas put is used to update the existing resource
+    public ResponseEntity<TodoDto> completeTodo(@PathVariable("id") Long todoId){
+        TodoDto updatedTodo = todoService.completeTodo(todoId);
+        return ResponseEntity.ok(updatedTodo);
+    }
+
+    // Build In Complete Todo REST API
+    @PatchMapping("{id}/in-complete")
+    public ResponseEntity<TodoDto> inCompleteTodo(@PathVariable("id") Long todoId){
+        TodoDto updatedTodo = todoService.inCompleteTodo(todoId);
+        return ResponseEntity.ok(updatedTodo);
+    }
+}
